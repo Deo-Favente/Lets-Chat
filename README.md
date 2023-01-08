8e899791f9097b91ba264486ec738113cd31d4a2
BuildTools <unconfigured@null.spigotmc.org>
2023-01-08 22:36:09 +0100

    🎉 Init repository

---
+++ README.md
@@ -0,0 +1,5 @@
+# 💬 LET'S CHAT
+
+Let's chat is a self-hosted chat service, based on Node.js with Socket.IO.
+
+It (will) contains discussions, nickname, welcome messages, auto-moderation and many others functionnalities !
---
+++ index.js
@@ -0,0 +1,14 @@
+// Modules
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app)
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+});
+
+server.listen(3000, () => {
+    console.log('Listening on *3000')
+});
+
